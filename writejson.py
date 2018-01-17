f5016e1323ee3d43693d2feeafd47eb29f9d2487
meganwhite <mfw247@gmail.com>
2018-01-17 14:41:33 -0500

    remove quotation marks from entries

--- writejson.py
+++ writejson.py
@@ -6,6 +6,16 @@ with open('song_list.csv',) as f:
     reader = csv.DictReader(f)
     rows = list(reader)
 
+def remove_quotes(csv_data):
+	for row in csv_data:
+		row['Song'] = row['Song'].replace('"','')
+		row['Year'] = int(row['Year'])
+	return csv_data
+
+
+new_csv = remove_quotes(rows)
+
+
 #write json
 with open('song_list.json', 'w') as f:
-    json.dump(rows, f, indent=2)
+    json.dump(new_csv, f, indent=2)
